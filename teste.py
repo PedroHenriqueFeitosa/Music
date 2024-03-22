84b653d017f7a8c4cb3b3ceaaf577ddee6f76278
Pedro Henrique Feitosa De Jesus <88751677+PedroHenriqueFeitosa@users.noreply.github.com>
2024-03-22 12:34:33 -0300

    Teste De Um Jogo De Adivinhação de Musica

---
+++ teste.py
@@ -0,0 +1,29 @@
+import random
+
+# Lista de músicas
+musicas = ["Perfeição", "Ainda E cedo", "Será", "Há Tempos", "Indios"]
+
+# Escolhendo aleatoriamente uma música da lista
+musica_sorteada = random.choice(musicas)
+
+# Exibindo a música sorteada
+print("A música sorteada é: ", musica_sorteada)
+
+import webbrowser
+
+# URL que deseja abrir
+url = "https://youtu.be/iCFUV9aJA_Y"
+url="https://www.youtube.com/watch?v=nM_gEzvhsM"
+# Abrir a URL no navegador padrão
+webbrowser.open(url)
+
+
+#idade=40
+#if idade >=18:
+    #print("você pode dirigir")
+#else:
+    #print("você nao pode dirigir")
+#if idade<=35:
+    #print("voce pode ter um carro novo")
+#else:
+   # print("Seu Carro Sera Velho")
